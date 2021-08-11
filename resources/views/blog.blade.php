dfc744790c0534fc540306c5d0b1c57fe488c1ef
alux <ahmadabd13741112@gmail.com>
2021-08-11 12:37:04 +0430

    Sass configed and is ready to use

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,9 @@
-<h1>Blog</h1>
-<x-Layout>
-    <p>Test Layout</p>
-</x-Layout>
+<html>
+    <link rel="stylesheet" href="/css/app.css">
+</html>
+<body>
+    <h1>Blog</h1>
+    <x-Layout>
+        <p>Test Layout</p>
+    </x-Layout>    
+</body>
