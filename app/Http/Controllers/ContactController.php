82a46f85a584e3b111f675403fc0776c33ed9491
alux <ahmadabd13741112@gmail.com>
2021-09-01 12:16:14 +0430

    ContactMe Clean Code

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,9 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
 use App\Http\Controllers\FlashMessage\Message;
 use App\Http\Requests\ContactMeRequest;
 use App\Models\Contact;
@@ -12,15 +9,25 @@ use Illuminate\Support\Facades\Auth;
 
 class ContactController extends Controller
 {
+
+    private $contactMeList = [
+        'email'     => null, 
+        'linkedin'  => null, 
+        'twitter'   => null, 
+        'instagram' => null, 
+        'github'    => null, 
+        'telegram'  => null
+    ];
+
     public function show_contactMe_to_client()
     {
         $icons = [
-            'linkedin' => 'entypo-linkedin-with-circle',
-            'email' => 'gmdi-email',
-            'telegram' => 'forkawesome-telegram',
+            'linkedin'  => 'entypo-linkedin-with-circle',
+            'email'     => 'gmdi-email',
+            'telegram'  => 'forkawesome-telegram',
             'instagram' => 'fab-instagram-square',
-            'twitter' => 'antdesign-twitter-circle',
-            'github' => 'bytesize-github'
+            'twitter'   => 'antdesign-twitter-circle',
+            'github'    => 'bytesize-github'
         ];
 
         $availableContactLinks = [];
@@ -45,54 +52,40 @@ class ContactController extends Controller
         return view('contactMe', ['availableContactLinks' => $availableContactLinks]);   
     }
 
+
     public function show_contactMe_edit()
     {
-        $laststoredContact = [
-            'email' => null, 
-            'linkedin' => null, 
-            'twitter' => null, 
-            'instagram' => null, 
-            'github' => null, 
-            'telegram' => null
-        ];
-
         // Get data from database to show as value in input tag if exist
         $contactsObject = Auth::user()->contact();
         $numberOfContactDBrows = $contactsObject->count();
         if ($numberOfContactDBrows > 0){
             $databseValues = $contactsObject->get()[0];
-    
-            $laststoredContact['email'] = $databseValues['email'];
-            $laststoredContact['linkedin'] = $databseValues['linkedin'];
-            $laststoredContact['twitter'] = $databseValues['twitter'];
-            $laststoredContact['instagram'] = $databseValues['instagram'];
-            $laststoredContact['github'] = $databseValues['github'];
-            $laststoredContact['telegram'] = $databseValues['telegram'];
+
+            $this->contactMeList['email'] = $databseValues['email'];
+            $this->contactMeList['linkedin'] = $databseValues['linkedin'];
+            $this->contactMeList['twitter'] = $databseValues['twitter'];
+            $this->contactMeList['instagram'] = $databseValues['instagram'];
+            $this->contactMeList['github'] = $databseValues['github'];
+            $this->contactMeList['telegram'] = $databseValues['telegram'];
         }
 
-        return view('contactMe_edit', ["values" => $laststoredContact]);
+        return view('contactMe_edit', ["values" => $this->contactMeList]);
     }
+
 
     public function store_contactMe(ContactMeRequest $request)
     {   
+        /** 
+         * Value of input lists should not be same
+         */
+
         $userId = auth()->user()->id;
         $validated = $request->validated();
 
-        $contactMeList = [
-            'email' => null, 
-            'linkedin' => null, 
-            'twitter' => null, 
-            'instagram' => null, 
-            'github' => null, 
-            'telegram' => null
-        ];
-
-        // Value of input lists should not be same
-
-        // store validated data as value in contactMeList array
+        // store validated data as value in this->contactMeList array
         foreach($validated as $contactWay){
             $validatedKey = array_keys($validated, $contactWay)[0];             
-            $contactMeList[$validatedKey] = $contactWay;
+            $this->contactMeList[$validatedKey] = $contactWay;
         }
 
         // delete old data from Contact model if exist
@@ -102,13 +95,13 @@ class ContactController extends Controller
         }  
 
         $contactMe = Contact::create([
-            'user_id' => $userId,
-            'email' => $contactMeList['email'],
-            'linkedin' => $contactMeList['linkedin'],
-            'twitter' => $contactMeList['twitter'],
-            'instagram' => $contactMeList['instagram'],
-            'github' => $contactMeList['github'],
-            'telegram' => $contactMeList['telegram']
+            'user_id'   => $userId,
+            'email'     => $this->contactMeList['email'],
+            'linkedin'  => $this->contactMeList['linkedin'],
+            'twitter'   => $this->contactMeList['twitter'],
+            'instagram' => $this->contactMeList['instagram'],
+            'github'    => $this->contactMeList['github'],
+            'telegram'  => $this->contactMeList['telegram']
         ]);
 
         if ($contactMe){
