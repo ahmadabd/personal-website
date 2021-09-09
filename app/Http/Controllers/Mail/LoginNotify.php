04c250b9500a7ce1912fd54bd16bdf8b54e963b6
alux <ahmadabd13741112@gmail.com>
2021-09-09 08:42:13 +0430

    Make a job for sending Mail in background using queue

--- app/Http/Controllers/Mail/LoginNotify.php
+++ app/Http/Controllers/Mail/LoginNotify.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers\Mail;
 
-use Illuminate\Support\Facades\Mail;
-use App\Mail\LoginNotificationMail;
+use App\Jobs\SendMail;
 
 class LoginNotify implements MailImp{
     public function send_mail(string $name, string $email){
         $date = date("Y/m/d H:i:s");
         $data = array( 'name' => $name, 'date' => $date );
         
-        Mail::to($email)->queue(new LoginNotificationMail($data));
+        $details = array(
+            "email" => $email,
+            "data"  => $data
+        );
+
+        SendMail::dispatch($details)->delay(5);
     }
 }
---
+++ app/Jobs/SendMail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+use Illuminate\Support\Facades\Mail;
+use App\Mail\LoginNotificationMail;
+
+class SendMail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $details;
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+
+    
+    public function handle()
+    {
+        $email = $this->details["email"];
+        $data = $this->details["data"];
+
+        Mail::to($email)->send(new LoginNotificationMail($data));
+    }
+}
