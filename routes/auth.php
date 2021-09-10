b6c304d3b8ca76327c918974887d956033b4bbe7
alux <ahmadabd13741112@gmail.com>
2021-09-10 09:15:27 +0430

    Code Code: routes/auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -4,21 +4,17 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(["LogUserAuthentication"])->group(function(){
+Route::middleware(["LogUserAuthentication", "guest"])->group(function(){
     Route::get('/register', [RegisteredUserController::class, 'create'])
-                ->middleware('guest')
                 ->name('register');
 
     Route::post('/register', [RegisteredUserController::class, 'store'])
-                ->middleware('guest')
                 ->name('postRegister');
 
     Route::get('/login', [AuthenticatedSessionController::class, 'create'])
-                    ->middleware('guest')
                     ->name('login');
 
     Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-                    ->middleware('guest')
                     ->name("postLogin");
 
 });
