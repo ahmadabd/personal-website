bedea7d804ec22a384e985cfb25c3e1a1894b3fb
alux <ahmadabd13741112@gmail.com>
2021-09-17 16:13:08 +0430

    Clean Code: delete useless comments

--- resources/views/components/dashboardnav.blade.php
+++ resources/views/components/dashboardnav.blade.php
@@ -3,7 +3,7 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        
+
         <!-- Get Picture from Database -->
         <nav class="navbar navbar-light bg-light">
             <div class="container">
@@ -13,11 +13,11 @@
                 </a>
             </div>
         </nav>
-        
+
         <a class="navbar-brand profileName {{ (request()->routeIs('change_profileName')) ? 'active' : '' }}" href="{{ route('change_profileName') }}">{{ $profileName ?? "Profile Name" }}</a>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
             <div class="container">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">   
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     @auth
                         <li class="nav-item">
                             <a class="nav-link {{ (request()->routeIs('edit_biography')) ? 'active' : '' }}" aria-current="page" href="{{ route('edit_biography') }}">About Me</a>
@@ -28,16 +28,6 @@
                         <li class="nav-item">
                             <a class="nav-link {{ (request()->routeIs('resume_editPage')) ? 'active' : '' }}" aria-current="page" href="{{ route('resume_editPage') }}">CV</a>
                         </li>
-
-                        {{-- <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                              CV
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                              <a class="dropdown-item {{ (request()->routeIs('resume_editPage')) ? 'active' : '' }}" aria-current="page" href="{{ route('resume_editPage') }}">Persian Cv</a>
-                              <a class="dropdown-item" href="#">English Cv</a>
-                            </div>
-                        </li> --}}
 
                         <li class="nav-item">
                             <a class="nav-link {{ (request()->routeIs('weblog_edit')) ? 'active' : '' }}" aria-current="page" href="{{ route('weblog_edit') }}">weblog</a>
