d9cbf8c6a866abdf7fcb38cc9b1797f29c2bb014
alux <ahmadabd13741112@gmail.com>
2021-09-22 15:22:29 +0330

    bug fixed

--- resources/views/Book/update.blade.php
+++ resources/views/Book/update.blade.php
@@ -4,13 +4,13 @@
     <h4>Book: {{ $book->title }}</h4>
     <div class="form-group">
         <label for="title">Title</label>
-        <input value="{{ $book->title }}" type="text" name="title" class="form-control" id="title" placeholder="Title ..." />
+        <input value="{{ $book->title }}" type="text" name="title" class="form-control" id="title" placeholder="Title ..." required />
         <br>
         <label for="descriptions">Description</label>
-        <textarea name="descriptions" class="form-control" id="descriptions" rows="20" placeholder="End each line with: \">{{ $book->descriptions }}</textarea>
+        <textarea name="descriptions" class="form-control" id="descriptions" rows="20" placeholder="End each line with: \" required>{{ $book->descriptions }}</textarea>
         <br>
         <label for="url">Book Url Address</label>
-        <input value="{{ $book->url }}" type="text" name="url" id="url" class="form-control" placeholder="http://...">
+        <input value="{{ $book->url }}" type="text" name="url" id="url" class="form-control" placeholder="http://..." required>
         <br>
         <label class="form-label" for="book_picture">Upload Books Picture:(800x600 px)|(Max size: 10 MB)</label>
         <input type="file" class="form-control" id="book_picture" name="book_picture" />
